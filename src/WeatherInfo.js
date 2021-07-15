d29a5368dcf18c2da9d4f88d475758b910056435
jordanbarbo <jordanbarbo@gmail.com>
2021-07-14 19:36:18 -0700

    Removed Max/Min Temp

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -6,25 +6,27 @@ import WeatherTemperature from "./WeatherTemperature";
 export default function WeatherInfo(props) {
     return (
     <div clasName="WeatherInfo">
-        <h1>{props.data.city}</h1>
+        
+          <div className="row">
+            <span className="col-7">
+            <h1>{props.data.city}</h1>
+            <ul>
+              <li> <FormattedDate date={props.data.date} /> </li>
+              <li className="text-capitalize">{props.data.description}</li>              
+            </ul>  
+            </span>
+            <span className="col-5">
+              <WeatherIcon code={props.data.icon} />
+            </span>
+
+
+          </div>
           <div className="row">
             <div className="col-7">
-              <ul>
-                <li>
-                  <FormattedDate date={props.data.date} />
-                  </li>
-                <li className="text-capitalize">{props.data.description}</li>
-              </ul>
-              <div>
-                <WeatherTemperature celsius={props.data.temperature} /> 
-              </div>
+              <WeatherTemperature celsius={props.data.temperature} /> 
             </div>
             <div className="col-5">
               <ul>
-                <li>
-                  <WeatherIcon code={props.data.icon} />
-                </li>
-                <li>{Math.round(props.data.maxTemp)} / {Math.round(props.data.minTemp)}</li>
                 <li>Wind: {Math.round(props.data.wind)} km/hr </li>
                 <li>Humidity: {props.data.humidity}% </li>
               </ul>         
