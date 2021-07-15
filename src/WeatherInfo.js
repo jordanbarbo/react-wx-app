a8180235283b85e4a47b716c3c23d4eeb2f9658f
jordanbarbo <jordanbarbo@gmail.com>
2021-07-14 18:32:15 -0700

    Added C | F links to display correct units

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
     return (
@@ -15,10 +16,7 @@ export default function WeatherInfo(props) {
                 <li className="text-capitalize">{props.data.description}</li>
               </ul>
               <div>
-                <span className="temperature">{Math.round(props.data.temperature)}</span>
-                <span className="units">
-                  °C | °F
-                </span>  
+                <WeatherTemperature celsius={props.data.temperature} /> 
               </div>
             </div>
             <div className="col-5">
