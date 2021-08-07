f8075be1eb9ac8590f962d2931243831f3e926f2
jordanbarbo <jordanbarbo@gmail.com>
2021-08-06 17:38:00 -0700

    Changed background and font colors

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="#005F99"
+      color="#FF449F"
       size={props.size}
       animate={true}
     />
